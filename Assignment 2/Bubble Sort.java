e19a02a270988808f5717449bc6c4eb12a7d7fc3
*ishitpareek* <ishitapareek15@gmail.com>
2026-02-17 22:21:52 +0530

    created assignment 2 and added bubble sort

---
+++ Assignment 2/Bubble Sort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static void main (String args[]) {
+		Scanner scan = new Scanner(System.in);
+		int size = scan.nextInt();
+
+		int arr[] = new int[size];
+
+		for (int i = 0; i < size; i++) 
+			arr[i] = scan.nextInt();
+
+		for (int i = 0; i < size - 1; i++) {
+			boolean hasBeenSwapped = false;
+			for (int j = 0; j < size - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					hasBeenSwapped = true;
+				}
+			}
+			if (hasBeenSwapped == false)
+				break;
+		}
+
+		for (int i = 0; i < size; i++) 
+			System.out.print(arr[i] + " ");  
+    }
+}  
